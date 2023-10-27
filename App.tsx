633c09aec7c33c1b6b693d942054eee7e5ca0e5c
m.peltier <m.peltier@meetic-corp.com>
2023-10-27 14:28:06 +0200

    initialise googleSignin

--- App.tsx
+++ App.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Button, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 
+GoogleSignin.configure({
+  webClientId:
+    '930529132291-tqud7m980o335grs9gj1djge17hbs6j3.apps.googleusercontent.com',
+});
 export const App = () => {
+  async function onGoogleButtonPress() {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+
+    console.log('idToken', idToken);
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<any>();
+
+  // Handle user state changes
+  function onAuthStateChanged(user: any) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  console.log('user', user);
+
+  if (initializing) return <Text>Loading</Text>;
+
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <Text>MyProject</Text>
+      <Button
+        title="Google Sign-In"
+        onPress={() =>
+          onGoogleButtonPress().then(() =>
+            console.log('Signed in with Google!'),
+          )
+        }
+      />
     </SafeAreaView>
   );
 };
