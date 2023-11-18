4f151e1c8d0f24dd303a6bcab96cdd5bf0365c50
m.peltier <m.peltier@meetic-corp.com>
2023-11-18 06:54:18 +0100

    refacto authentication

--- App.tsx
+++ App.tsx
@@ -1,41 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {Button, SafeAreaView, StyleSheet, Text} from 'react-native';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
+import {onGoogleButtonPress} from './ios/src/authentication/googleSignin';
+import {useAuthentication} from './ios/src/authentication/authentication';
 
 GoogleSignin.configure({
   webClientId:
     '930529132291-tqud7m980o335grs9gj1djge17hbs6j3.apps.googleusercontent.com',
 });
 export const App = () => {
-  async function onGoogleButtonPress() {
-    // Check if your device supports Google Play
-    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
-    // Get the users ID token
-    const {idToken} = await GoogleSignin.signIn();
-
-    // Create a Google credential with the token
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-    // Sign-in the user with the credential
-    return auth().signInWithCredential(googleCredential);
-  }
-
-  const logout = () => {
-    auth()
-      .signOut()
-      .then(() => console.log('User signed out!'));
-  };
-
-  // Set an initializing state whilst Firebase connects
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState<any>();
-
-  // Handle user state changes
-  function onAuthStateChanged(user: any) {
-    setUser(user);
-    if (initializing) setInitializing(false);
-  }
+  const {initializing, user, onAuthStateChanged, logout} = useAuthentication();
 
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
